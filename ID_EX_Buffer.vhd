58cc977463ffd8815ba1304f931e4f7839845454
Daniel Eskandar <danieleskandar@yahoo.com>
2020-04-20 14:34:50 +0200

    Changed register and multiplexers to generic entities

--- ID_EX_Buffer.vhd
+++ ID_EX_Buffer.vhd
@@ -44,90 +44,44 @@ entity ID_EX_Buffer is
 end ID_EX_Buffer;
 
 architecture ID_EX_Buffer_arch of ID_EX_Buffer is
+	component reg is 
+		generic(
+			n : integer
+		);
+
+		port(
+			clk : in std_logic;
+		 	reset : in std_logic;
+		 	en : in std_logic;
+			d : in std_logic_vector(n-1 downto 0);
+			q : out std_logic_vector(n-1 downto 0)
+		);
+	end component;
+
 	component reg1 is 
 		port(
-			   clk : in std_logic;
-			   reset : in std_logic;
-			   en : in std_logic;
-			   d : in std_logic;
-			   q : out std_logic
-	    	);
-	end component;
-
-	component reg2 is 
-		port(
-			   clk : in std_logic;
-			   reset : in std_logic;
-			   en : in std_logic;
-			   d : in std_logic_vector(1 downto 0);
-			   q : out std_logic_vector(1 downto 0)
-	   	 );
-	end component;
-
-	component reg3 is 
-		port(
-			   clk : in std_logic;
-			   reset : in std_logic;
-			   en : in std_logic;
-			   d : in std_logic_vector(2 downto 0);
-			   q : out std_logic_vector(2 downto 0)
-		    );
-	end component;
-
-	component reg5 is 
-		port(
-			   clk : in std_logic;
-			   reset : in std_logic;
-			   en : in std_logic;
-			   d : in std_logic_vector(4 downto 0);
-			   q : out std_logic_vector(4 downto 0)
-		    );
-	end component;
-
-	component reg6 is 
-		port(
-			   clk : in std_logic;
-			   reset : in std_logic;
-			   en : in std_logic;
-			   d : in std_logic_vector(5 downto 0);
-			   q : out std_logic_vector(5 downto 0)
-		    );
-	end component;
-
-	component reg11 is 
-		port(
-			   clk : in std_logic;
-			   reset : in std_logic;
-			   en : in std_logic;
-			   d : in std_logic_vector(10 downto 0);
-			   q : out std_logic_vector(10 downto 0)
-		    );
-	end component;
-
-	component reg32 is 
-		port(
-			   clk : in std_logic;
-			   reset : in std_logic;
-			   en : in std_logic;
-			   d : in std_logic_vector(31 downto 0);
-			   q : out std_logic_vector(31 downto 0)
-		    );
+			clk : in std_logic;
+			reset : in std_logic;
+			en : in std_logic;
+			d : in std_logic;
+			q : out std_logic
+	  	);
 	end component;
 begin
-	WB_reg : reg5 port map(clk,reset,en,D_WB,Q_WB);
-	MEM_reg : reg11 port map(clk,reset,en,D_MEM,Q_MEM);
-	EX_reg : reg11 port map(clk,reset,en,D_EX,Q_EX);
-	opcode_reg : reg6 port map(clk,reset,en,D_opcode,Q_opcode);
-	INPS_reg : reg2 port map(clk,reset,en,D_INPS,Q_INPS);
+	WB_reg : reg generic map(5) port map(clk,reset,en,D_WB,Q_WB);
+	MEM_reg : reg generic map(11) port map(clk,reset,en,D_MEM,Q_MEM);
+	EX_reg : reg generic map(11) port map(clk,reset,en,D_EX,Q_EX);
+	opcode_reg : reg generic map(6) port map(clk,reset,en,D_opcode,Q_opcode);
+	INPS_reg : reg generic map(2) port map(clk,reset,en,D_INPS,Q_INPS);
 	int_reg : reg1 port map(clk,reset,en,D_int,Q_int);
-	PC_reg : reg32 port map(clk,reset,en,D_PC,Q_PC);
-	PCnext_reg : reg32 port map(clk,reset,en,D_PCnext,Q_PCnext);
-	RD1_reg : reg32 port map(clk,reset,en,D_RD1,Q_RD1);
-	RD2_reg : reg32 port map(clk,reset,en,D_RD2,Q_RD2);
-	IMMe_reg : reg32 port map(clk,reset,en,D_IMMe,Q_IMMe);
-	EAe_reg : reg32 port map(clk,reset,en,D_EAe,Q_EAe);
-	WR1_reg : reg3 port map(clk,reset,en,D_WR1,Q_WR1);
-	WR2_reg : reg3 port map(clk,reset,en,D_WR2,Q_WR2);
-	RR1_reg : reg3 port map(clk,reset,en,D_RR1,Q_RR1);
-	RR2_reg : reg3 port map(clk,reset,en,D_RR2,Q_RR2);	
+	PC_reg : reg generic map(32) port map(clk,reset,en,D_PC,Q_PC);
+	PCnext_reg : reg generic map(32) port map(clk,reset,en,D_PCnext,Q_PCnext);
+	RD1_reg : reg generic map(32) port map(clk,reset,en,D_RD1,Q_RD1);
+	RD2_reg : reg generic map(32) port map(clk,reset,en,D_RD2,Q_RD2);
+	IMMe_reg : reg generic map(32) port map(clk,reset,en,D_IMMe,Q_IMMe);
+	EAe_reg : reg generic map(32) port map(clk,reset,en,D_EAe,Q_EAe);
+	WR1_reg : reg generic map(3) port map(clk,reset,en,D_WR1,Q_WR1);
+	WR2_reg : reg generic map(3) port map(clk,reset,en,D_WR2,Q_WR2);
+	RR1_reg : reg generic map(3) port map(clk,reset,en,D_RR1,Q_RR1);
+	RR2_reg : reg generic map(3) port map(clk,reset,en,D_RR2,Q_RR2);	
 end ID_EX_Buffer_arch;
